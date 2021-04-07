42c1441b0a6593bce8c938a3324068c6b475ae3f
Sehinde Oluyede <82111688+Olasehinde92@users.noreply.github.com>
2021-04-07 14:23:55 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-pip install pandas
-pip install requests
+pip3 install -r requirements.txt -t ./src/python/modules
 python3 src/python/PROFIT.py "https://gist.githubusercontent.com/bobbae/b4eec5b5cb0263e7e3e63a6806d045f2/raw/279b794a834a62dc108fc843a72c94c49361b501/data.csv"
